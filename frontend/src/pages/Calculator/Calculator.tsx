ca164795cada37b14deb1ffafb553ad82b74b6f0
bjalyona <aly0nasm@mail.ru>
2025-06-11 14:45:50 +0300

    footprint stats in a separate component

--- frontend/src/pages/Calculator/Calculator.tsx
+++ frontend/src/pages/Calculator/Calculator.tsx
@@ -6,23 +6,8 @@ import {
   type FootprintResult,
 } from "../../utils/calculateFootprint";
 
-import {
-  PieChart,
-  Pie,
-  Cell,
-  Tooltip,
-  ResponsiveContainer,
-  Legend,
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-} from "recharts";
-import PlanetDisplay from "./PlanetDisplay";
 import { questions } from "../../utils/questions";
-
-const COLORS = ["#84a98c", "#52796f", "#cad2c5", "#354f52"];
+import FootprintStats from "../../components/FootprintStats/FootprintStats";
 
 const initialData: UserInput = {
   meatMealsPerWeek: 0,
@@ -90,21 +75,6 @@ export default function Calculator() {
   const [current, setCurrent] = useState(0);
   const [answers, setAnswers] = useState<UserInput>(initialData);
   const [result, setResult] = useState<FootprintResult | null>(null);
-  const co2ComparisonData = result
-    ? [
-        { name: "Твой результат", value: result.co2KgPerYear },
-        { name: "Средний результат по России", value: 4000 },
-        { name: "Средний результат по миру", value: 5000 },
-      ]
-    : [];
-
-  const ghaComparisonData = result
-    ? [
-        { name: "Твой результат", value: result.gha },
-        { name: "Средний результат по России", value: 4.4 },
-        { name: "Средний результат по миру", value: 2.7 },
-      ]
-    : [];
 
   const handleAnswer = (value: number | boolean) => {
     const key = questions[current].key as keyof UserInput;
@@ -116,15 +86,6 @@ export default function Calculator() {
       setResult(result);
     }
   };
-
-  const categoryBreakdownChartData =
-  result?.breakdownByCategory
-    ? Object.entries(result.breakdownByCategory).map(([name, value]) => ({
-        name,
-        value,
-      }))
-    : [];
-
 
   return (
     <div className="container">
@@ -138,58 +99,7 @@ export default function Calculator() {
             />
           </div>
         ) : (
-          <div className="charts-grid">
-            <div className="chart-container">
-              <PlanetDisplay planets={result.planets} />
-            </div>
-
-            <div className="chart-container">
-              <h3>Вклад разных сфер в твой углеродный след</h3>
-              <ResponsiveContainer width="100%" height={300}>
-                <PieChart>
-                  <Pie
-                    data={categoryBreakdownChartData}
-                    dataKey="value"
-                    nameKey="name"
-                    cx="50%"
-                    cy="50%"
-                    outerRadius={100}
-                    label
-                  >
-                    {categoryBreakdownChartData.map((_, index) => (
-                      <Cell key={index} fill={COLORS[index % COLORS.length]} />
-                    ))}
-                  </Pie>
-                  <Tooltip />
-                  <Legend />
-                </PieChart>
-              </ResponsiveContainer>
-            </div>
-
-            <div className="comparison-charts">
-              <h3>Сравнение CO₂ (kg/year)</h3>
-              <ResponsiveContainer width="100%" height={250}>
-                <BarChart data={co2ComparisonData}>
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis dataKey="name" />
-                  <YAxis />
-                  <Tooltip />
-                  <Bar dataKey="value" fill="#52796f" />
-                </BarChart>
-              </ResponsiveContainer>
-
-              <h3>Сравнение GHA (Глобальные гектары)</h3>
-              <ResponsiveContainer width="100%" height={250}>
-                <BarChart data={ghaComparisonData}>
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis dataKey="name" />
-                  <YAxis />
-                  <Tooltip />
-                  <Bar dataKey="value" fill="#84a98c" />
-                </BarChart>
-              </ResponsiveContainer>
-            </div>
-          </div>
+          <FootprintStats result={result} />
         )}
       </div>
     </div>
